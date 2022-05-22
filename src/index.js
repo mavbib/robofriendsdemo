82de25976a81d127cde50bd7d7491a582c392a50
mav <mavquart@gmail.com>
2022-05-22 15:53:41 -0400

    update createrender2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement); 
 
-ReactDOM.createRoot(
-   document.getElementById('root')).render(
-   <App />);
+root.render(
+   <App />
+   );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
