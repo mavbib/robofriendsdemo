4647e7ba28543b6b440aca4fbbf4aa41f67b14b2
mav <mavquart@gmail.com>
2022-05-23 10:47:32 -0400

    Updates

---
+++ precache-manifest.a543beafbb5fcaa74fbce8cc4fa173a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "07481fe0e671be9a626e",
+    "url": "/robofriendsdemo/static/css/main.406a964a.chunk.css"
+  },
+  {
+    "revision": "07481fe0e671be9a626e",
+    "url": "/robofriendsdemo/static/js/main.07481fe0.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robofriendsdemo/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "f35463a524282ce875d7",
+    "url": "/robofriendsdemo/static/css/2.fab6bfee.chunk.css"
+  },
+  {
+    "revision": "f35463a524282ce875d7",
+    "url": "/robofriendsdemo/static/js/2.f35463a5.chunk.js"
+  },
+  {
+    "revision": "01c5af8fc749d6a7191e",
+    "url": "/robofriendsdemo/static/js/runtime~main.01c5af8f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsdemo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "838abd86c99b8290f5fd12178496fcfe",
+    "url": "/robofriendsdemo/index.html"
+  }
+];
